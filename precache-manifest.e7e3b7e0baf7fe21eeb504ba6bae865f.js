9d5ea1ad65ca0f7fb6276888ef23a8065f005323
Guilherme Lawless <guilherme@nano.org>
2020-01-27 18:59:54 +0000

    Updates

---
+++ precache-manifest.e7e3b7e0baf7fe21eeb504ba6bae865f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3653499b346499a599065cdae91e51d",
+    "url": "/impersonator-checker/index.html"
+  },
+  {
+    "revision": "5e89f32564896b40db9b",
+    "url": "/impersonator-checker/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "936d7bc1fcca9c4a9074",
+    "url": "/impersonator-checker/static/js/2.86ff57d9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/impersonator-checker/static/js/2.86ff57d9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5e89f32564896b40db9b",
+    "url": "/impersonator-checker/static/js/main.90416d07.chunk.js"
+  },
+  {
+    "revision": "31767665bf02b538bc67",
+    "url": "/impersonator-checker/static/js/runtime-main.9e56f947.js"
+  }
+]);
