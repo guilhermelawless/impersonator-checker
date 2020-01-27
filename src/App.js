cb51ebe3ac1ead3e740f4929d3f8a03d5ac2c9fe
Guilherme Lawless <guilherme@nano.org>
2020-01-27 18:53:31 +0000

    Impersonator checker

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class ImpersonatorCheck extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {user1: '', user2: '', message: ''};
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Discord Impersonator Check</h3>
+        <form onSubmit={this.handleSubmit}>
+          <label>User1</label>
+          <input type="text" name='user1' value={this.state.user1.value} onChange={this.handleChange} />
+          
+          <label>User2</label>
+          <input type="text" name='user2' value={this.state.user2.value} onChange={this.handleChange} />            
+        </form>
+        {this.state.message}
+      </div>
+    );
+  }
+
+  handleChange(e) {
+    this.setState({[e.target.name]: e.target.value }, () => this.handleSubmit(e));
+  }
+
+  handleSubmit(e) {
+    var message = "";
+    if (this.state.user1 === this.state.user2) {
+      message = "Same person";
+    } else if (this.state.user1.length !== this.state.user2.length) {
+      message = "Different lengths";
+    } else {
+      message = "NOT THE SAME PERSON";
+    }
+    this.setState({message: message})
+  }
 }
 
-export default App;
+export default ImpersonatorCheck;
